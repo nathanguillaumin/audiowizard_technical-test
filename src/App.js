ff8948b83de76a54b795e6649255acbd0c005334
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-07-10 17:02:19 +0200

    adds and styles topnav

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SecondPage from './pages/SecondPage';
 import ThirdPage from './pages/ThirdPage';
+import TopNav from './components/TopNav';
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Switch>
-        <Route exact path='/'><HomePage /></Route>
-        <Route exact path='/second'><SecondPage /></Route>
-        <Route exact path='/third'><ThirdPage /></Route>
-      </Switch>
+        <TopNav />
+        <Switch>
+          <Route exact path='/'><HomePage /></Route>
+          <Route exact path='/second'><SecondPage /></Route>
+          <Route exact path='/third'><ThirdPage /></Route>
+        </Switch>
       </div>
     </Router>
   );
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import '../styles/topnav.css'
 
 const TopNav = () => {
     return (
-        <div>
-            
+        <div className='topnav-container'>
+            <div className='logo-container'>
+                <img className='logo-audiowizard' src={require('../images/logo_audiowizard.png')} alt='logo AudioWizard' />
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Thibaut Gressier
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
         </div>
     );
 };
